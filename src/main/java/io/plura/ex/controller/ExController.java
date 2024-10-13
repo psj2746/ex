a018cd3c74dbfdee8913622d27801313b556ed3b
simon <simon@qubitsec.com>
2024-10-14 01:12:41 +0900

    cors 대응

--- src/main/java/io/plura/ex/controller/ExController.java
+++ src/main/java/io/plura/ex/controller/ExController.java
@@ -1,6 +1,7 @@
 package io.plura.ex.controller;
 
 import io.plura.ex.dto.ExDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ex")
 public class ExController {
+    @CrossOrigin(origins = "*")
     @GetMapping("/hello")
     public ExDto hello() {
-        return new ExDto(0, "ex> ok #1");
+        return new ExDto(0, "ex> ok #2");
     }
 
     @GetMapping("/nop")
